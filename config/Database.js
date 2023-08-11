06ac570977af630b537d93aa30013fd58a2b9d7d
ben23I <139108360+ben23I@users.noreply.github.com>
2023-08-11 11:23:14 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
 
 const { DataTypes } = Sequelize;
-const db = new Sequelize("test", "test", "123456", {
-	host: "127.0.0.1",
+// const db = new Sequelize("test", "test", "123456", {
+const db = new Sequelize("test", "test2", "Aa!123456", {
+	// host: "127.0.0.1",
+	host: "192.168.123.62",
 	dialect: "mysql",
 });
 
